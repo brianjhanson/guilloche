8b8511477c886ef560b167e318c21d13bf33e85b
Brian Hanson <brian@brianhanson.net>
2016-08-01 09:03:30 -0500

    Initial commit

--- guilloche.js
+++ guilloche.js
@@ -1,42 +1,30 @@
-
 function Guilloche() {
-	var _steps = 300+Math.round(Math.random()*2400); // Divide a circle this many times
-    var _R = 50; // The major ripple
-	var _rr = 0.25; // The minor ripple
-	var _p = 25; // Radius type effect
-	var _m = 1; // Angle multiplier
-	var _amplitude = 4.5; // Scale of final drawing
-	var _opacity = 0.2; // Line opacity
-	var _color1 = 0x000000; // Line start colour
-	var _color2 = 0x000000; // Line end colour
-	var _thickness = 0.5; // Line thickness
-	var section_length = 1; // Number of sections for each line
-	var deg2rad = Math.PI / 180; // Factor to convert degs to radians
-	var _colorCycle = 1; // The number of times the colour repeats
-	var colorPalette = []; // For storing a gradient array of colours
+  this.steps = 1800; // Divide a circle this many times
+  this.majorRipple = 50; // The major ripple
+  this.minorRipple = 0.25; // The minor ripple
+  this.radiusEffect = 25; // Radius type effect
+  this.angleMultiplier = 1; // Angle multiplier
+  this.amplitude = 6; // Scale of final drawing
+  this.sectionLength = 1; // Number of sections for each line
 
 	this.plot = function(ctx) {
 		var l;
 		var x, y, ox, oy;
 		var sl = 0;
 		var theta = 0;
-		var thetaStep = 2 * Math.PI / _steps;
-		var s = (_R + _rr) / _rr;
-		var rR = _rr + _R;
-		var rp = _rr + _p;
+		var thetaStep = 2 * Math.PI / this.steps;
+		var s = (this.majorRipple + this.minorRipple) / this.minorRipple;
+		var rR = this.minorRipple + this.majorRipple;
+		var rp = this.minorRipple + this.radiusEffect;
 
-		var cpl = colorPalette.length;
-		var c = _color1;
+		for(var t = 0; t <= this.steps; t++) {
+			x = rR * Math.cos(this.angleMultiplier * theta) + rp * Math.cos(s * this.angleMultiplier * theta);
+			y = rR * Math.sin(this.angleMultiplier * theta) + rp * Math.sin(s * this.angleMultiplier * theta);
 
-		for(var t = 0; t <= _steps; t++) {
-			x = rR * Math.cos(_m * theta) + rp * Math.cos(s * _m * theta);
-			y = rR * Math.sin(_m * theta) + rp * Math.sin(s * _m * theta);
-
-			x *= _amplitude;
-			y *= _amplitude;
+			x *= this.amplitude;
+			y *= this.amplitude;
 
 			if(sl == 0) {
-				c = (_color1 == _color2 || cpl < 2) ? _color1 : colorPalette[t % cpl];
 
 				ctx.beginPath();
 
@@ -50,7 +38,7 @@ function Guilloche() {
 			} else {
 				// Append to line section
 				ctx.lineTo(x, y);
-                ctx.stroke();
+        ctx.stroke();
 			}
 
 			ox = x;
@@ -58,133 +46,62 @@ function Guilloche() {
 			sl++;
 			theta += thetaStep;
 
-			if(sl == section_length) sl = 0;
+			if(sl == this.sectionLength) sl = 0;
 		}
 	}
-
-
-	this.setColorPalette = function() {
-		//colorPalette = ColorUtil.blendArray(_color1, _color2, ((_steps / section_length) - 1), false)
-		//colorPalette = colorPalette.concat(ColorUtil.blendArray(_color2, _color1, ((_steps / section_length) - 1), false))
-	}
-
-
-	/**
-	 *	Get and set amplitude
-	 */
-	this.get_amplitude = function() {
-		return _amplitude;
-	}
-	this.set_amplitude = function(value) {
-		if(value !== _amplitude) _amplitude = value;
-	}
-
-	/**
-	 *	Get and set steps
-	 */
-	this.get_steps = function() {
-		return _steps;
-	}
-	this.set_steps = function(value) {
-		if(value !== _steps) _steps = value;
-	}
-
-	/**
-	 *	Set colour cycle steps
-	 */
-	this.get_colorCycle = function() {
-		return _colorCycle;
-	}
-	this.set_colorCycle = function(value) {
-		if(value !== _colorCycle) {
-			_colorCycle = value;
-			//setColorPalette();
-		}
-	}
-
-	/**
-	 *	Get and set R value - the Major ripple effect
-	 */
-	this.get_R = function() {
-		return _R;
-	}
-	this.set_R = function(value) {
-		if(value !== _R) _R = value;
-	}
-
-	/**
-	 *	Get and set r value - the minor ripple effect
-	 */
-	this.get_rr = function() {
-		return _rr;
-	}
-	this.set_rr = function(value) {
-		if(value !== _rr) _rr = value;
-	}
-
-	/**
-	 *	Get and set p - the radius type effect
-	 */
-	this.get_p = function() {
-		return _p;
-	}
-	this.set_p = function(value) {
-		if(value !== _p) _p = value;
-	}
-
-	/**
-	 *	Get and set m value - angle multiplier
-	 */
-	this.get_m = function() {
-		return _m;
-	}
-	this.set_m = function(value) {
-		if(value !== _m) _m = value;
-	}
-
-	/**
-	 *	Get and set opacity
-	 */
-	this.get_opacity = function() {
-		return _opacity;
-	}
-	this.set_opacity = function(value) {
-		if(value !== _opacity) _opacity = value;
-	}
-
-	/**
-	 *	Get and set thickness
-	 */
-	this.get_thickness = function() {
-		return _thickness;
-	}
-	this.set_thickness = function(value) {
-		if(value !== _thickness) _thickness = value;
-	}
-
-	/**
-	 *	Get and set colour
-	 */
-	this.get_color1 = function() {
-		return _color1;
-	}
-	this.set_color1 = function(value) {
-		if(value !== _color1) {
-			_color1 = value;
-			//setColorPalette();
-		}
-	}
-	this.get_color2 = function() {
-		return _color1;
-	}
-	this.set_color2 = function(value) {
-		if(value !== _color2) {
-			_color2 = value;
-			//setColorPalette();
-		}
-	}
-
-
 	return this;
-
 }
+
+function getContext() {
+  var canvas = document.getElementById('c');
+  var ctx = canvas.getContext('2d');
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  ctx.translate((ctx.canvas.width/2)+0.5, (ctx.canvas.height/2)+0.5);
+
+  var grad = ctx.createLinearGradient(0, 0, 300, 0);
+  grad.addColorStop(0, "#ce7630");
+  grad.addColorStop(1, "#eba758");
+
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = 0.5;
+  ctx.lineJoin = "round";
+
+  return ctx;
+}
+
+function initGui(item) {
+  var gui = new dat.GUI();
+  gui.add(item, 'steps', 300, 4800);
+  gui.add(item, 'majorRipple', 0, 150);
+  gui.add(item, 'minorRipple', 0, 1.5).step(0.05);
+  // gui.add(item, 'angleMultiplier', 1, 6).step(0.25);
+  gui.add(item, 'amplitude', 1, 10);
+  gui.add(item, 'radiusEffect', 1, 100);
+  // gui.add(item, 'sectionLength', 1, 2);
+
+  return gui;
+}
+
+function clearContext(ctx) {
+  ctx.save();
+  ctx.setTransform(1,0,0,1,0,0);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.restore();
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var ctx = getContext();
+  var g = Guilloche();
+  var gui = initGui(g);
+
+  gui.__controllers.forEach(function(controller) {
+    controller.onChange(function(value) {
+      clearContext(ctx);
+      g.plot(ctx);
+    });
+  });
+
+  g.plot(ctx);
+  ctx.createPattern(ctx);
+});
